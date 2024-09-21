91c0e8e6ecbe25a0c4f27df8df4770eb11a393ea
oflatt <oflatt@gmail.com>
2024-09-21 14:14:10 -0700

    bump version

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,37 @@ use bevy::{
     render::render_resource::{AsBindGroup, ShaderRef},
 };
 
+use bevy::{
+    color::palettes::basic::YELLOW,
+    core_pipeline::core_2d::Transparent2d,
+    math::FloatOrd,
+    prelude::*,
+    render::{
+        mesh::{GpuMesh, Indices, MeshVertexAttribute},
+        render_asset::{RenderAssetUsages, RenderAssets},
+        render_phase::{
+            AddRenderCommand, DrawFunctions, PhaseItemExtraIndex, SetItemPipeline,
+            ViewSortedRenderPhases,
+        },
+        render_resource::{
+            BlendState, ColorTargetState, ColorWrites, Face, FragmentState, FrontFace,
+            MultisampleState, PipelineCache, PolygonMode, PrimitiveState, PrimitiveTopology,
+            RenderPipelineDescriptor, SpecializedRenderPipeline, SpecializedRenderPipelines,
+            TextureFormat, VertexBufferLayout, VertexFormat, VertexState, VertexStepMode,
+        },
+        texture::BevyDefault,
+        view::{ExtractedView, ViewTarget, VisibleEntities},
+        Extract, Render, RenderApp, RenderSet,
+    },
+    sprite::{
+        extract_mesh2d, DrawMesh2d, Material2dBindGroupId, Mesh2dHandle, Mesh2dPipeline,
+        Mesh2dPipelineKey, Mesh2dTransforms, MeshFlags, RenderMesh2dInstance, SetMesh2dBindGroup,
+        SetMesh2dViewBindGroup, WithMesh2d,
+    },
+    utils::EntityHashMap,
+};
+use std::f32::consts::PI;
+
 fn main() {
     App::new()
         .add_plugins((DefaultPlugins, MaterialPlugin::<CustomMaterial>::default()))
@@ -42,3 +73,48 @@ impl Material for CustomMaterial {
         "animate_shader.wgsl".into()
     }
 }
+
+fn star(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+    let mut star = Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::RENDER_WORLD,
+    );
+
+    // lets make a box
+    let mut v_pos = vec![
+        [0.0, 0.0, 0.0],
+        [0.0, 100.0, 0.0],
+        [100.0, 100.0, 0.0],
+        [100.0, 0.0, 0.0],
+    ];
+
+    // Set the position attribute
+    star.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
+    // And a RGB color attribute as well
+    let mut v_color: Vec<u32> = vec![LinearRgba::BLACK.as_u32()];
+    v_color.extend_from_slice(&[LinearRgba::from(YELLOW).as_u32(); 10]);
+    star.insert_attribute(
+        MeshVertexAttribute::new("Vertex_Color", 1, VertexFormat::Uint32),
+        v_color,
+    );
+
+    let mut indices = vec![0, 1, 2, 0, 2, 3];
+    star.insert_indices(Indices::U32(indices));
+
+    // We can now spawn the entities for the star and the camera
+    commands.spawn((
+        // We use a marker component to identify the custom colored meshes
+        MountainMesh,
+        // The `Handle<Mesh>` needs to be wrapped in a `Mesh2dHandle` to use 2d rendering instead of 3d
+        Mesh2dHandle(meshes.add(star)),
+        // This bundle's components are needed for something to be rendered
+        SpatialBundle::INHERITED_IDENTITY,
+    ));
+
+    // Spawn the camera
+    commands.spawn(Camera2dBundle::default());
+}
+
+/// A marker component for colored 2d meshes
+#[derive(Component, Default)]
+pub struct MountainMesh;
