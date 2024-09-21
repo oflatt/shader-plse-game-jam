d17a79d9ea3104d77ecc65881a38424d8cbdcb8d
oflatt <oflatt@gmail.com>
2024-09-21 15:22:57 -0700

    custom mesh generated using loop

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<MountainMaterial>>,
+    mut std_materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
     // Create and save a handle to the mesh.
@@ -43,6 +44,7 @@ fn setup(
             mesh: cube_mesh_handle,
             transform: Transform::from_xyz(0.0, 0.5, 0.0),
             material: materials.add(MountainMaterial {}),
+            //material: std_materials.add(StandardMaterial { ..default() }),
             ..default()
         },
         Mountain {},
@@ -116,6 +118,40 @@ fn input_handler(
 }
 
 fn create_cube_mesh() -> Mesh {
+    let mut vertex_positions = vec![];
+    let mut uv_positions = vec![];
+    let mut triangles = vec![];
+    let mut normals = vec![];
+
+    let x_max = 100;
+    let y_max = 100;
+    let last_index = (x_max * y_max) - 1;
+    for xi in 0..x_max {
+        for yi in 0..y_max {
+            vertex_positions.push([
+                (xi as f32) / (x_max as f32),
+                (yi as f32) / (y_max as f32),
+                (xi as f32) / (x_max as f32) + (yi as f32) / (y_max as f32),
+            ]);
+            normals.push([0.0, 0.0, 1.0]);
+
+            // we make squares, so two triangles per index
+            let index = xi * y_max + yi;
+            let index_right = (xi + 1) * y_max + yi;
+            let index_down = xi * y_max + yi + 1;
+            let index_down_right = (xi + 1) * y_max + yi + 1;
+
+            if index_down_right <= last_index {
+                triangles.extend(vec![index, index_down_right, index_right]);
+                triangles.extend(vec![index, index_down, index_down_right]);
+            }
+        }
+    }
+
+    for pos in vertex_positions.iter() {
+        uv_positions.push([pos[0], pos[1]])
+    }
+
     // Keep the mesh data accessible in future frames to be able to mutate it in toggle_texture.
     Mesh::new(
         PrimitiveTopology::TriangleList,
@@ -127,132 +163,12 @@ fn create_cube_mesh() -> Mesh {
         // The camera coordinate space is right-handed x-right, y-up, z-back. This means "forward" is -Z.
         // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
         // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-        vec![
-            // top (facing towards +y)
-            [-0.5, 0.5, -0.5], // vertex with index 0
-            [0.5, 0.5, -0.5],  // vertex with index 1
-            [0.5, 0.5, 0.5],   // etc. until 23
-            [-0.5, 0.5, 0.5],
-            // bottom   (-y)
-            [-0.5, -0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            // right    (+x)
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-            [0.5, 0.5, -0.5],
-            // left     (-x)
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, 0.5, -0.5],
-            // back     (+z)
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            // forward  (-z)
-            [-0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [0.5, -0.5, -0.5],
-        ],
+        vertex_positions,
     )
-    // Set-up UV coordinates to point to the upper (V < 0.5), "dirt+grass" part of the texture.
-    // Take a look at the custom image (assets/textures/array_texture.png)
-    // so the UV coords will make more sense
-    // Note: (0.0, 0.0) = Top-Left in UV mapping, (1.0, 1.0) = Bottom-Right in UV mapping
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            // Assigning the UV coords for the top side.
-            [0.0, 0.2],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 0.2],
-            // Assigning the UV coords for the bottom side.
-            [0.0, 0.45],
-            [0.0, 0.25],
-            [1.0, 0.25],
-            [1.0, 0.45],
-            // Assigning the UV coords for the right side.
-            [1.0, 0.45],
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            // Assigning the UV coords for the left side.
-            [1.0, 0.45],
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            // Assigning the UV coords for the back side.
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            [1.0, 0.45],
-            // Assigning the UV coords for the forward side.
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            [1.0, 0.45],
-        ],
-    )
-    // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
-    // the surface.
-    // Normals are required for correct lighting calculations.
-    // Each array represents a normalized vector, which length should be equal to 1.0.
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![
-            // Normals for the top side (towards +y)
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            // Normals for the bottom side (towards -y)
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            // Normals for the right side (towards +x)
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            // Normals for the left side (towards -x)
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            // Normals for the back side (towards +z)
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            // Normals for the forward side (towards -z)
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-        ],
-    )
-    // Create the triangles out of the 24 vertices we created.
-    // To construct a square, we need 2 triangles, therefore 12 triangles in total.
-    // To construct a triangle, we need the indices of its 3 defined vertices, adding them one
-    // by one, in a counter-clockwise order (relative to the position of the viewer, the order
-    // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
-    // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
-    // further examples and the implementation of the built-in shapes.
-    .with_inserted_indices(Indices::U32(vec![
-        0, 3, 1, 1, 3, 2, // triangles making up the top (+y) facing side.
-        4, 5, 7, 5, 6, 7, // bottom (-y)
-        8, 11, 9, 9, 11, 10, // right (+x)
-        12, 13, 15, 13, 14, 15, // left (-x)
-        16, 19, 17, 17, 19, 18, // back (+z)
-        20, 21, 23, 21, 22, 23, // forward (-z)
-    ]))
+    // make uv the same as vertex positions XD
+    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uv_positions)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+    .with_inserted_indices(Indices::U32(triangles))
 }
 
 // Function that changes the UV mapping of the mesh, to apply the other texture.
